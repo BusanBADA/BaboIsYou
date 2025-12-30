5cc32b996240a04c70e61d8f0917086f963e08fc
BCA-PC12 <BCA-PC12@DESKTOP-822FT01>
2025-12-30 16:14:36 +0900

    Reapply "고정소수점 물리엔진/그리드/레벨 저장 기능 추가"
    This reverts commit 74279a8df21489977468a686b8da37635eb0cab4.

--- Project/Project/Core/Constants.h
+++ Project/Project/Core/Constants.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "Fixed64.h"
-
+//////////////////////////////
+//  //
+//////////////////////////////
 namespace BABO::Core {
+	using namespace BABO::Math;
 	constexpr int TILE_SIZE_INT = 30;
-	const Fixed64 TILE_SIZE_F = Fixed64(TILE_SIZE_INT);
-	const Fixed64 TILE_HALF_F = TILE_SIZE_F / Fixed64(2);
+	const Fix64 TILE_SIZE_F = Fix64(TILE_SIZE_INT);
+	const Fix64 TILE_HALF_F = TILE_SIZE_F / Fix64(2);
+	const double MS_PER_UPDATE = 1.0 / 60.0;
 }
