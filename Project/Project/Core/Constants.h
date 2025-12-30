74279a8df21489977468a686b8da37635eb0cab4
BCA-PC12 <BCA-PC12@DESKTOP-822FT01>
2025-12-30 16:13:18 +0900

    Revert "고정소수점 물리엔진/그리드/레벨 저장 기능 추가"
    This reverts commit bb0b1e2f0476f22f4236b2c2da7bef59b8fa5837.

--- Project/Project/Core/Constants.h
+++ Project/Project/Core/Constants.h
@@ -1,12 +1,8 @@
 #pragma once
 #include "Fixed64.h"
-//////////////////////////////
-//  //
-//////////////////////////////
+
 namespace BABO::Core {
-	using namespace BABO::Math;
 	constexpr int TILE_SIZE_INT = 30;
-	const Fix64 TILE_SIZE_F = Fix64(TILE_SIZE_INT);
-	const Fix64 TILE_HALF_F = TILE_SIZE_F / Fix64(2);
-	const double MS_PER_UPDATE = 1.0 / 60.0;
+	const Fixed64 TILE_SIZE_F = Fixed64(TILE_SIZE_INT);
+	const Fixed64 TILE_HALF_F = TILE_SIZE_F / Fixed64(2);
 }
--- Project/Project/Core/Fixed64.h
+++ Project/Project/Core/Fixed64.h
@@ -1,84 +1,17 @@
-
 #pragma once
 #include <cstdint>
 #include <cmath>
-#include <iostream>
+using Fixed64 = int64_t;
+const Fixed64 ONE = 65536; // 1.0
+const Fixed64 HALF_TILE = 32768; // 0.5
+inline Fixed64 ToFixed(float f) { return (Fixed64)(f * ONE); }
+inline Fixed64 ToFixed(int i) { return (Fixed64)i * ONE; }
+inline float ToFloat(Fixed64 x) { return (float)x / ONE; }
 
+struct Vector2Fixed {
+    Fixed64 x, y;
 
-#ifdef _MSC_VER
-#include <intrin.h>
-#endif
-
-namespace BABO::Math {
-    constexpr int64_t ONE_RAW = 1LL << 32;
-
-    struct Fix64 {
-        int64_t value;
-
-        constexpr Fix64() : value(0) {}
-        struct RawTag {};
-        constexpr Fix64(int64_t raw, RawTag) : value(raw) {}
-        constexpr Fix64(int v) : value(static_cast<int64_t>(v) << 32) {}
-        constexpr Fix64(float v) : value(static_cast<int64_t>(v* ONE_RAW)) {}
-
-        Fix64 operator+(const Fix64& o) const { return { value + o.value, RawTag{} }; }
-        Fix64 operator-(const Fix64& o) const { return { value - o.value, RawTag{} }; }
-
-
-        Fix64 operator*(const Fix64& o) const {
-#ifdef _MSC_VER
-
-            __int64 high;
-            unsigned __int64 low = _mul128(value, o.value, &high);
-
-
-            int64_t res = (int64_t)((low >> 32) | ((unsigned __int64)high << 32));
-            return { res, RawTag{} };
-#else
-
-            __int128_t res = static_cast<__int128_t>(value) * o.value;
-            return { static_cast<int64_t>(res >> 32), RawTag{} };
-#endif
-        }
-
-        Fix64 operator/(const Fix64& o) const {
-            if (o.value == 0) return { 0, RawTag{} };
-
-#ifdef _MSC_VER
-
-            __int64 remainder;
-            __int64 result = _div128(value >> 32, value << 32, o.value, &remainder);
-            return { result, RawTag{} };
-#else
-
-            __int128_t num = static_cast<__int128_t>(value) << 32;
-            return { static_cast<int64_t>(num / o.value), RawTag{} };
-#endif
-        }
-
-        bool operator==(const Fix64& o) const { return value == o.value; }
-        bool operator!=(const Fix64& o) const { return value != o.value; }
-        bool operator<(const Fix64& o) const { return value < o.value; }
-        bool operator>(const Fix64& o) const { return value > o.value; }
-
-
-        float ToFloat() const { return static_cast<float>(value) / ONE_RAW; }
-        int ToInt() const { return static_cast<int>(value >> 32); }
-        int64_t Raw() const { return value; }
-
-        static Fix64 Zero() { return { 0, RawTag{} }; }
-        static Fix64 One() { return { ONE_RAW, RawTag{} }; }
-
-        static Fix64 Abs(Fix64 v) {
-            return { v.value < 0 ? -v.value : v.value, RawTag{} };
-        }
-    };
-
-    struct Vector2Fixed {
-        Fix64 x, y;
-        Vector2Fixed operator+(const Vector2Fixed& o) const { return { x + o.x, y + o.y }; }
-        Vector2Fixed operator-(const Vector2Fixed& o) const { return { x - o.x, y - o.y }; }
-        Vector2Fixed operator*(Fix64 s) const { return { x * s, y * s }; }
-        bool operator==(const Vector2Fixed& o) const { return x == o.x && y == o.y; }
-    };
-}
+    Vector2Fixed operator+(const Vector2Fixed& other) const { return { x + other.x, y + other.y }; }
+    Vector2Fixed operator-(const Vector2Fixed& other) const { return { x - other.x, y - other.y }; }
+    Vector2Fixed operator*(Fixed64 scalar) const { return { (x * scalar) / ONE, (y * scalar) / ONE }; }
+};
