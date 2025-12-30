c983e593d2b9bebbdea4d07f706454c5808fcc7f
BCA-PC12 <BCA-PC12@DESKTOP-822FT01>
2025-12-30 18:12:51 +0900

    pulling main branch for merging

--- Project/Project/Level.h
+++ Project/Project/Level.h
@@ -7,7 +7,7 @@
 #include "InstancedObject.h"
 #include "TileObject.h"
 
-// BABO 핵심 시스템 포함
+
 #include "Core/Grid.h"
 #include "Core/RuleManager.h"
 #include "Core/Physics.h"
@@ -27,19 +27,17 @@ public:
 private:
     void CreateDefaultLevel(const std::string& path);
     void SyncToLogicGrid();
-    void SaveCurrentLevel(); // 라벨 포함 저장 로직
-
-    // 핵심 시스템 멤버
+    void SaveCurrentLevel(); 
     BABO::World::GridSystem m_gridSystem;
     BABO::Logic::RuleManager m_ruleManager;
     std::string m_levelPath = "level1.babo";
 
-    // 객체 포인터
+
     Player* player = nullptr;
     GameObject* cursor = nullptr;
     TileObject* frObj00 = nullptr;
 
-    // 배경 및 플래그 객체 (Init에서 초기화)
+
     BackgroundObject* bgObj00, * bgObj01, * bgObj02, * bgObj03, * bgObj04, * bgObj05, * bgObj06, * bgObj07, * bgObj08;
     BackgroundObject* bgObj00Sub, * bgObj01Sub, * bgObj02Sub, * bgObj03Sub, * bgObj04Sub, * bgObj05Sub, * bgObj06Sub, * bgObj07Sub, * bgObj08Sub;
     FlagObject* flag00, * flag01, * flag02, * flag03, * flag04;
