cbd1950865fea7d5e17184d909c06b18e5d74312
최신우 <sinwoovj0107@gmail.com>
2025-12-31 14:04:02 +0900

    solve Gravity logic

--- Project/Project/TileManager.h
+++ Project/Project/TileManager.h
@@ -48,7 +48,7 @@ public:
 
     bool CheckBlankPosition(const glm::vec2& cord, ObjectiveType moveType);
 
-    bool CheckIsFixedPosition(const glm::vec2& cord, ObjectiveType moveType);
+    bool CheckStarPosition(const glm::vec2& cord, ObjectiveType moveType);
 
     bool CheckPushable(TileObject& tileObj, const glm::vec2& dir);
 
